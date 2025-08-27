31c533ad20428da6e1fb8c5c24c8da2806871a72
Erik Eiesland <erik.eiesland@nav.no>
2025-08-27 09:40:43 +0200

    Første commit

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { Application } from "jsr:@oak/oak/application";
+import { Router } from "jsr:@oak/oak/router";
+
+const utlån = new Map<string, any>();
+utlån.set("03128221750", {
+  hjelpemidler: [],
+});
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello, DigiHoT!";
+  })
+  .get("/utlan", (context) => {
+    context.response.body = Array.from(utlån.values());
+  })
+  .get("/utlan/:id", (context) => {
+    if (utlån.has(context?.params?.id)) {
+      context.response.body = utlån.get(context.params.id);
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
